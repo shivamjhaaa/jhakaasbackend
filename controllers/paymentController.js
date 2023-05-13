fb0d00af3ee66eeed12e8bd7ad4f84d80245230c
shivam jha <imshivamj@gmail.com>
2023-05-13 11:34:41 +0530

    remove to delete one

--- controllers/paymentController.js
+++ controllers/paymentController.js
@@ -97,7 +97,7 @@ export const cancelSubscription = catchAsyncError(async(req,res,next)=>{
         refund = true;
     }
 
-    await payment.remove();
+    await payment.deleteOne();
     user.subscription.id = undefined;
     user.subscription.status = undefined;
 
